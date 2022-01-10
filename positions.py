2300ef7cb66e0ae6f9ddc2d3c79e25b69628be67
Andre Petersen <mail@andrepetersen.de>
2022-01-10 22:34:36 +0100

    Visualization lap table

---
+++ positions.py
@@ -0,0 +1,24 @@
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash()
+
+df = pd.read_csv(
+    "data/lap_table.csv",
+    sep=";"
+)
+
+df = pd.melt(df, id_vars="Driver", value_vars=df.columns[1:], var_name="Lap", value_name="Pos")
+
+df.dropna(how="any", inplace=True)
+
+fig = px.line(df, x="Lap", y="Pos", color="Driver", title='Track Position')
+
+app.layout = html.Div([dcc.Graph(id="life-exp-vs-gdp", figure=fig)])
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
