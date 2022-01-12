65311bd1e40f94d0c67c93ebff799e577af9c1e4
Andre Petersen <mail@andrepetersen.de>
2022-01-12 19:08:08 +0100

    Updated visuals

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ import pandas as pd
 
 from functions import get_df
 
-app = dash.Dash()
+app = dash.Dash(__name__)
 
 server = app.server
 
 app.layout = html.Div([
+    html.H1("Circuit Superstars Statistics Dashboard"),
+    html.P("This is a community project. Administration and coding by Dremet and data aggregation by McVizn."),
     dcc.Dropdown(
         id='league',
         options=[
@@ -20,7 +22,9 @@ app.layout = html.Div([
         value='superstars'
     ),
     dcc.Graph(id="driver_race_points"),
-    dcc.Graph(id="lap_table")
+    #dcc.Graph(id="team_race_points"),
+    dcc.Graph(id="lap_table"),
+    html.P("(c)")
 ])
 
 @app.callback(
@@ -29,8 +33,28 @@ app.layout = html.Div([
 )
 def update_race_points_graph(league):
     df = get_df(league, "driver_race_points")
+    df["color"] = df["racepoints"]
+    df.loc[df["color"].isnull(), "color"] = 0
+    df["color"] = df["color"].astype(int)
+    number_racers = len(df.index)
 
-    fig = px.line(df, x="driver name", y="racepoints", title='Race Points')
+    standard_colors = []
+
+    for i in range(number_racers-3):
+        standard_colors.append("blue")
+
+    colors = ["gold","silver","bronze"]+standard_colors
+
+    color_discrete_map = {}
+    for points, color in zip(df["color"].values, colors):
+        points_save = points if points >= 0 else 0
+
+        color_discrete_map[int(points_save)] = color
+
+    print(df["color"].astype(int).values)
+    print(color_discrete_map)
+
+    fig = px.bar(df, x="driver name", y="racepoints", color="color", title='Race Points', color_discrete_map=color_discrete_map)
 
     return fig
 
