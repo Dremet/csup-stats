668d88cc69d682c075f6fa97fe6abbf17c2341c4
Andre Petersen <mail@andrepetersen.de>
2022-01-15 18:56:54 +0100

    Environment variable handling on heroku

--- connection.py
+++ connection.py
@@ -1,13 +1,19 @@
+import os
 import psycopg2
 from decouple import config
 
 
 class Connection:
     def __init__(self) -> None:
-        self.host, self.dbname, self.user, self.password = config("PGHOST"),config("PGDBNAME"),config("PGUSER"),config("PGPASSWORD")
+        if os.uname()[1] == "LAPTOP-1O7S9IJH":
+            self.host, self.dbname, self.user, self.password = config("PGHOST"),config("PGDBNAME"),config("PGUSER"),config("PGPASSWORD")
     
     def __enter__(self):
-        self.conn = psycopg2.connect(f"host={self.host} dbname={self.dbname} user={self.user} password={self.password}")
+        if os.uname()[1] == "LAPTOP-1O7S9IJH":
+            self.conn = psycopg2.connect(f"host={self.host} dbname={self.dbname} user={self.user} password={self.password}")
+        else:
+            self.conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+
         self.conn.set_session(autocommit=True)
 
         return self.conn
