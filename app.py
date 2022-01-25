feba50f1ee9a22c9c3053ccdeb65ba5aa0bc7609
Andre Petersen <mail@andrepetersen.de>
2022-01-25 19:30:40 +0100

    Major application rework and updates

--- app.py
+++ app.py
@@ -1,14 +1,375 @@
+from click import style
 import dash
-from dash import dcc
-from dash import html
+from dash import dcc, html, dash_table as dt
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
-from functions import get_df,read_driver_data
+from plotly.graph_objs import Layout
 
-app = dash.Dash(__name__)
+from functions import get_df, read_driver_data, get_team_standings_cumulative, get_driver_standings_cumulative, get_race_meta_data_and_race_results
+
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.LUMEN])
+app.config.suppress_callback_exceptions=True
 
 server = app.server
+transparent_layout = Layout()
+transparent_layout = Layout(
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(225,225,225,100)'
+    )
+#  bs-light  253,246,227
+#  bs-info   38,139,210
+
+
+
+###############################################
+###         HEADER & NAVIGATION             ###
+###############################################
+
+header = html.Div([
+        dbc.Row(html.H1("Circuit Superstars Statistics Dashboard")),
+        dbc.Card(
+            [
+                dbc.CardBody(
+                    [
+                        html.H4("Notes", className="card-title"),
+                        html.P(
+                            "This is a community project by Dremet and McVizn. Data is entered manually by hand, which is why there will always be some kind of delay after races are finished.", 
+                            className="card-text"
+                        )
+                    ]
+                ),
+                dbc.CardFooter("If you have suggestions regarding improvements of this dashboard, feel free to contact Dremet on Discord."),
+            ], 
+            color="info", 
+            inverse=True,
+            style={"margin-bottom": "10px"}
+        )
+])
+
+tabs = dbc.CardHeader(
+        dbc.Tabs(
+            [
+                dbc.Tab(label="Season Standings", tab_id="season_standings"),
+                dbc.Tab(label="Race Results", tab_id="race_results"),
+                dbc.Tab(label="Drivers", tab_id="drivers")
+            ],
+            id="tabs", 
+            active_tab="season_standings"
+        )
+    )
+
+
+
+content = html.Div(id="tab-content")
+
+footer = html.Div(
+    dbc.Alert("Feel free to use the graphs wherever you like, but please mention https://csup-stats.herokuapp.com/ as the source.", 
+    color="dark", 
+    style={"margin-top": "10px"})
+)
+
+app.layout = dbc.Container([
+    header,
+    dbc.Card([
+        tabs,
+        dbc.CardBody(content)
+    ]),
+    footer
+])
+
+@app.callback(
+    dash.dependencies.Output("tab-content", "children"),
+    dash.dependencies.Input("tabs", "active_tab")
+)
+def render_tab_content(active_tab):
+    if active_tab == "season_standings":
+        return html.Div([
+                    dbc.Row(
+                        [dbc.Col(
+                            dcc.Dropdown(
+                                id='season_cs',
+                                options=[
+                                    {'label': 'ICSTC', 'value': 'ICSTC'},
+                                    #{'label': 'ICSES', 'value': 'ICSES'}
+                                ],
+                                value='ICSTC'
+                            )
+                        ),
+                        dbc.Col(
+                            dcc.Dropdown(
+                                id='season_league',
+                                options=[
+                                    {'label': 'Superstars', 'value': 'Superstars'},
+                                    {'label': 'Experts', 'value': 'Experts'},
+                                    {'label': 'Pros', 'value': 'Pros'},
+                                    {'label': 'Semi Pros', 'value': 'Semi Pros'},
+                                    {'label': 'Amateur', 'value': 'Amateur'}
+                                ],
+                                value='Superstars'
+                            )
+                        ),
+                        dbc.Col(
+                            dcc.Dropdown(
+                                id='season',
+                                options=[
+                                    {'label': '1', 'value': '1'},
+                                    {'label': '2', 'value': '2'}
+                                ],
+                                value='2'
+                            )
+                        )]
+                    ),
+                    dbc.Row([
+                        dbc.Col(dcc.Graph(id="team_standings")),
+                        dbc.Col(dcc.Graph(id="driver_standings"))
+                    ])
+                ])
+    elif active_tab == "race_results":
+        return html.Div([dbc.Row([
+                        dbc.Col(
+                            dcc.Dropdown(
+                                id='race_cs',
+                                options=[
+                                    {'label': 'ICSTC', 'value': 'ICSTC'},
+                                    #{'label': 'ICSES', 'value': 'ICSES'}
+                                ],
+                                value='ICSTC'
+                            )
+                        ),
+                        dbc.Col(
+                            dcc.Dropdown(
+                                id='race_league',
+                                options=[
+                                    {'label': 'Superstars', 'value': 'Superstars'},
+                                    {'label': 'Experts', 'value': 'Experts'},
+                                    {'label': 'Pros', 'value': 'Pros'},
+                                    {'label': 'Semi Pros', 'value': 'Semi Pros'},
+                                    {'label': 'Amateur', 'value': 'Amateur'}
+                                ],
+                                value='Superstars'
+                            )
+                        ),
+                        dbc.Col(
+                            dcc.Dropdown(
+                                id='race_season',
+                                options=[
+                                    #{'label': '1', 'value': '1'},
+                                    {'label': '2', 'value': '2'}
+                                ],
+                                value='2'
+                            )
+                        ),
+                        dbc.Col(
+                            dcc.Dropdown(
+                                id='race',
+                                options=[
+                                    {'label': '2022-01-09', 'value': '20220109'},
+                                    {'label': '2022-01-16', 'value': '20220116'},
+                                    {'label': '2022-01-23', 'value': '20220123'}
+                                ],
+                                value='20220123'
+                            )
+                        )
+                        ]
+                    ),
+                    dbc.Row([html.H3("Race Details")], style={"margin-top":"10px"}),
+                    dbc.Row([
+                        dbc.Col(
+                            dt.DataTable(
+                                id='race_table', 
+                                style_cell={
+                                    'whiteSpace': 'pre-line'
+                                }
+                            )
+                        )
+                    ]),
+
+                    dbc.Row([html.H3("Race Results")], style={"margin-top":"10px"}),
+                    dbc.Row([
+                        dbc.Col(
+                            dt.DataTable(
+                                id='driver_race_table', 
+                                style_cell={
+                                    'whiteSpace': 'pre-line'
+                                }
+                            )
+                        )
+                        #dcc.Graph(id="lap_table")
+                    ]),
+
+                    
+                        
+                ])
+    elif active_tab == "drivers":
+        return html.Div([
+                    dbc.Row([
+                        dbc.Col(update_drivers_region_graph()),
+                        dbc.Col(update_drivers_device_graph())
+                        ]
+                    )
+                    #,
+                    # dbc.Row(dcc.Dropdown(
+                    #     id='region',
+                    #     options=[
+                    #         {'label': 'World', 'value': 'world'},
+                    #         {'label': 'Europe', 'value': 'EU'},
+                    #         {'label': 'North America', 'value': 'NA'},
+                    #         {'label': 'South America', 'value': 'SA'}
+                    #     ],
+                    #     value='world'
+                    # )),
+                    # dbc.Row([
+                        
+                    # ])
+                ])
+    
+    return html.P("No tab selected")
+    #misc_tab = dbc.Tab(label = "Misc", children = [])
+
+###############################################
+###          Dropdown Callbacks             ###
+###############################################
+
+@app.callback(
+    [dash.dependencies.Output("season_league", "options"), dash.dependencies.Output("season_league", "value")], 
+    dash.dependencies.Input("season_cs", "value")
+)
+def update_available_leagues(season_cs):
+    if season_cs == "ICSTC":
+        return [
+                {'label': 'Superstars', 'value': 'Superstars'},
+                {'label': 'Experts', 'value': 'Experts'},
+                {'label': 'Pros', 'value': 'Pros'},
+                {'label': 'Semi Pros', 'value': 'Semi Pros'},
+                {'label': 'Amateur', 'value': 'Amateur'}
+            ], "Superstars"
+    elif season_cs == "ICSES":
+        return [
+                {'label': 'Group A', 'value': 'A'},
+                {'label': 'Group B', 'value': 'B'},
+                {'label': 'Group C', 'value': 'C'}
+            ], "A"
+    else:
+        return [], None
+
+###############################################
+###            Season Standings             ###
+###############################################
+
+@app.callback(
+    dash.dependencies.Output('team_standings', 'figure'),
+    [dash.dependencies.Input('season_cs', 'value'), dash.dependencies.Input('season_league', 'value'), dash.dependencies.Input('season', 'value')],
+)
+def update_team_standings_graph(season_cs, season_league, season):
+    df = get_team_standings_cumulative(cs=season_cs, league=season_league, season=season)
+    df["track_car"] = df["r_track"]+"\n"+df["r_car"]
+
+    df.sort_values("e_date", inplace=True, ascending=True)
+    #print(df)
+    fig = px.line(df, x="track_car", y="points_cum", color="t_name", title='Team Standings')
+    fig.layout = transparent_layout
+
+    return fig
+
+
+@app.callback(
+    dash.dependencies.Output('driver_standings', 'figure'),
+    [dash.dependencies.Input('season_cs', 'value'), dash.dependencies.Input('season_league', 'value'), dash.dependencies.Input('season', 'value')],
+)
+def update_driver_standings_graph(season_cs, season_league, season):
+    df = get_driver_standings_cumulative(cs=season_cs, league=season_league, season=season)
+    df["track_car"] = df["r_track"]+"\n"+df["r_car"]
+
+    df.sort_values("e_date", inplace=True, ascending=True)
+
+    fig = px.line(df, x="track_car", y="points_cum", color="d_name", title='Driver Standings')
+    fig.layout = transparent_layout
+
+    return fig
+
+
+###############################################
+###            Race Results                 ###
+###############################################
+
+@app.callback(
+    [dash.dependencies.Output("driver_race_table", "columns"), dash.dependencies.Output("driver_race_table", "data"),
+    dash.dependencies.Output("race_table", "columns"), dash.dependencies.Output("race_table", "data")],
+    [
+        dash.dependencies.Input('race_cs', 'value'), dash.dependencies.Input('race_league', 'value'), 
+        dash.dependencies.Input('race_season', 'value'), dash.dependencies.Input('race', 'value')
+    ]
+)
+def update_drivers_race_table(race_cs, race_league, race_season, race):
+    df_meta, df_race = get_race_meta_data_and_race_results(cs=race_cs, league=race_league, season=race_season, event=race)
+    
+    df_meta.rename(columns={
+        "e_date" : "Date", 
+        "r_track" : "Track", 
+        "r_version" : "Version",
+        "r_reversed" : "Reversed", 
+        "r_laps" : "Laps", 
+        "r_car" : "Car", 
+        "r_tire_wear" : "Tire Wear", 
+        "r_fuel_consumption" : "Fuel", 
+        "r_damage_cars" : "Car Damage", 
+        "r_damage_environment" : "Env Damage",
+        "r_drafting" : "Drafting",
+        "r_has_reversed_grid" : "Reverse Grid",
+        "r_details_were_announced" : "Announced"
+        }, 
+        inplace=True
+    )
+
+    df_race.rename(columns={
+        "d_name" : "Driver", 
+        "t_tag" : "Team Tag", 
+        "q_lap_time_seconds" : "Quali Time",
+        "quali_points" : "Quali Points", 
+        "rr_fastest_lap_seconds" : "Fastest Lap", 
+        "fastest_lap_points" : "Points Fastest Lap", 
+        "rr_race_time_seconds" : "Race Time", 
+        "race_points" : "Race Points", 
+        "points" : "Points"
+        }, 
+        inplace=True
+    )
+
+    def sec_to_readable_time(secs):
+        if np.isnan(secs):
+            return "-"
+        secs = float(secs)
+        
+        str_min = str(int(np.floor(secs/60)))
+        str_secs = str(int(np.floor(secs%60))).zfill(2)
+        str_msecs = str(int(np.round(secs%1*1000))).zfill(3)
+
+        return f"{str_min}:{str_secs}.{str_msecs}"
+    
+    df_race["Race Time"] = df_race["Race Time"].apply(sec_to_readable_time)
+    
+    df_race.sort_values("Race Points", ascending=False, inplace=True)
+
+    results_columns = [{"name": i, "id": i} for i in df_race.columns]
+    results_data = df_race.to_dict('records')
+
+    meta_columns = [{"name": i, "id": i} for i in df_meta.columns]
+    meta_data = df_meta.to_dict('records')
+
+    return results_columns, results_data, meta_columns, meta_data
+
+# @app.callback(
+#     dash.dependencies.Output('lap_table', 'figure'),
+#     dash.dependencies.Input('league', 'value')
+# )
+# def update_lap_table_graph(league):
+#     df = get_df(league, "lap_table")
+#     fig = px.line(df, x="Lap", y="Pos", color="driver name", title='Track Position')
+#     fig.layout = transparent_layout
+#     return fig
 
 
 ###############################################
@@ -22,80 +383,29 @@ def update_drivers_region_graph():
 
     fig = px.sunburst(df_grouped, path=["d_two_letter_continent_code", "d_two_letter_country_code"], values="counts")
 
-    fig.update_layout(paper_bgcolor="#D3D3D3")
+    fig.layout = transparent_layout
 
     return dcc.Graph(id="region_overview", figure=fig)
 
+def update_drivers_device_graph():
+    df = read_driver_data()
 
-app.layout = html.Div([
-    html.H1("Circuit Superstars Statistics Dashboard"),
-    html.P("This is a community project. Administration and coding by Dremet and data aggregation by McVizn."),
-    dcc.Tabs([
-        dcc.Tab(label = "Race Results", children = [
-            dcc.Dropdown(
-                id='league',
-                options=[
-                    {'label': 'ICSTC Superstars', 'value': 'superstars'},
-                    {'label': 'ICSTC Experts', 'value': 'experts'}
-                ],
-                value='superstars'
-            ),
-            dcc.Graph(id="driver_race_points"),
-            #dcc.Graph(id="team_race_points"),
-            dcc.Graph(id="lap_table")
-        ]),
-        dcc.Tab(label = "Drivers", children = [
-            update_drivers_region_graph(),
-            dcc.Dropdown(
-                id='region',
-                options=[
-                    {'label': 'World', 'value': 'world'},
-                    {'label': 'Europe', 'value': 'EU'},
-                    {'label': 'North America', 'value': 'NA'},
-                    {'label': 'South America', 'value': 'SA'}
-                ],
-                value='world'
-            ),
-            dcc.Graph(id="steering_device"),
-            dcc.Graph(id="drivers_table")
-        ])
-    ]),
-    html.P("(c)")
-])
+    df["d_steering_device"] = df["d_steering_device"].replace(
+        {
+            "controller" : "Controller",
+            "keyboard" : "Keyboard",
+            "alternating" : "Both",
+            "" : "?"
+        }
+    )
 
-###############################################
-###            Race Results                 ###
-###############################################
+    df_grouped = df.groupby(['d_steering_device']).size().reset_index(name='counts')
 
-@app.callback(
-    dash.dependencies.Output('driver_race_points', 'figure'),
-    dash.dependencies.Input('league', 'value')
-)
-def update_race_points_graph(league):
-    df = get_df(league, "driver_race_points")
+    fig = px.sunburst(df_grouped, path=["d_steering_device"], values="counts")
 
-    fig = px.bar(df, x="driver name", y="racepoints", title='Race Points')
-    return fig
+    fig.layout = transparent_layout
 
-# @app.callback(
-#     dash.dependencies.Output('team_race_points', 'figure'),
-#     dash.dependencies.Input('league', 'value')
-# )
-# def update_team_race_points_graph(league):
-#     df = get_df(league, "team_race_points")
-
-#     fig = px.bar(df, x="team name", y="racepoints", title='Team Race Points')
-#     return fig
-
-@app.callback(
-    dash.dependencies.Output('lap_table', 'figure'),
-    dash.dependencies.Input('league', 'value')
-)
-def update_lap_table_graph(league):
-    df = get_df(league, "lap_table")
-    fig = px.line(df, x="Lap", y="Pos", color="driver name", title='Track Position')
-    return fig
-
+    return dcc.Graph(id="region_overview", figure=fig)
 
 
 
--- functions.py
+++ functions.py
@@ -1,7 +1,16 @@
 from math import exp
 import pandas as pd 
-
+import numpy as np
 from connection import Connection
+
+from pathlib import Path
+
+PATH_SQL_TEMPLATES=Path("sql/")
+
+def read_sql_template(sql_file):
+    file_path = PATH_SQL_TEMPLATES / sql_file
+    return file_path.read_text().replace('\n', ' ')
+
 
 def get_df(league, cat=None):
 
@@ -55,6 +64,91 @@ def read_driver_data():
     return df
 
 
+def get_driver_results(cs, league, season):
+    with Connection() as conn:
+        sql = read_sql_template("get_results_by_c_l_s.sql")
+        cur = conn.cursor()
+        
+        cur.execute(sql, dict(zip(["cs","league","season"], [cs, league, season])))
+        columns = [desc[0] for desc in cur.description]
+
+        df = pd.DataFrame(cur.fetchall(), columns = columns)
+        
+        # drop rows with r_id = NaN
+        # this can happen if future events are in the database f.e.
+        df.dropna(subset=["r_id"], inplace=True)
+        
+        assert len(df.loc[df["e_uses_event_points"],:]) == 0, "Can only calculate points up until now, if e_uses_event_points is False."
+
+        # calculate points for each race attendand 
+        df["points"] = 0
+        df["quali_points"] = 0
+        df["fastest_lap_points"] = 0
+        df["has_fastest_race_lap"] = False
+        df["has_fastest_quali_lap"] = False
+        
+        fastest_laps = df.groupby(df.r_id)[["rr_fastest_lap_seconds", "q_lap_time_seconds"]].min()
+        
+        for r_id, row in fastest_laps.iterrows():
+            df.loc[(df.r_id == r_id) & (df.rr_fastest_lap_seconds == row["rr_fastest_lap_seconds"]), "has_fastest_race_lap"] = True
+            df.loc[(df.r_id == r_id) & (df.q_lap_time_seconds == row["q_lap_time_seconds"]), "has_fastest_quali_lap"] = True
+        
+        df.loc[df.has_fastest_race_lap, "fastest_lap_points"] = df.loc[df.has_fastest_race_lap, "e_points_for_fastest_lap"]
+        df.loc[df.has_fastest_quali_lap, "quali_points"] = df.loc[df.has_fastest_quali_lap, "e_points_for_pole"]
+
+        #print(df[["rr_fastest_lap_seconds", "q_lap_time_seconds","has_fastest_race_lap", "has_fastest_quali_lap", "points"]])
+
+        # assign race points depending on the e_points_pos_X columns, if a race position is given
+        df["race_points"] = df.apply(lambda x: x[f"e_points_pos_{int(x['rr_position'])}"] if not np.isnan(x['rr_position']) else 0, axis=1)
+        
+        # add race points to previously calculated points from fastest lap and pole
+        df["points"] = df["quali_points"] + df["fastest_lap_points"] + df["race_points"]
+
+        # we want to calculate cumulative points in the order in which the races took place
+        # so we sort by date ascending
+        # group the points by driver and race
+        # do magic
+        # and then calculate the cumsum and reverse the index 
+        df_points_cum = df.sort_values("e_date", ascending=True)[["r_id", "d_id", "points"]].groupby(["d_id", "r_id"]).sum().groupby(level=0).cumsum().reset_index()
+
+        # create points_cum column using a join
+        df = df.set_index(["r_id", "d_id"]).join(df_points_cum.set_index(["r_id", "d_id"]), rsuffix="_cum").reset_index()
+
+        # calculate overall sum of points
+        df["points_sum"] = df[["d_id", "points"]].groupby(["d_id"]).transform("sum")["points"]
+        
+        # return only columns needed for other functions
+        return df
+
+
+def get_driver_standings_cumulative(cs, league, season):
+    #print(get_driver_results(cs, league, season)[["d_name", "r_track", "r_car", "points_cum"]])
+    return get_driver_results(cs, league, season)[["d_name", "r_track", "r_car", "e_date", "points_cum"]]
+
+
+def get_team_standings_cumulative(cs, league, season):
+    df = get_driver_results(cs, league, season)[["t_name", "r_track", "r_car", "e_date", "points_cum"]]
+
+    return df.groupby(["r_track", "r_car","t_name","e_date"]).sum().reset_index()
+
+def get_race_meta_data_and_race_results(cs, league, season, event):
+    df = get_driver_results(cs, league, season)
+    df["e_date_str"] = pd.to_datetime(df["e_date"]).dt.strftime("%Y%m%d")
+    df = df.loc[df["e_date_str"]==event,:]
+    
+    df_meta = df[[
+        "e_date","r_track", "r_version", "r_reversed", "r_laps", "r_car", "r_tire_wear", "r_fuel_consumption", "r_damage_cars", "r_damage_environment", "r_drafting", "r_has_reversed_grid", "r_details_were_announced"
+    ]].iloc[[0],:]
+
+    df_race = df[[
+        "d_name", "t_tag", "q_lap_time_seconds", "quali_points", "rr_fastest_lap_seconds", "fastest_lap_points", "rr_race_time_seconds", "race_points", "points"
+    ]]
+
+    return df_meta, df_race
+    
 
 if __name__ == "__main__":
-    get_df("experts", "team_race_points")
+    #get_df("experts", "team_race_points")
+    #print(get_driver_standings("ICSTC","Superstars","2"))
+    get_driver_results("ICSTC","Superstars","2")
+    pass
